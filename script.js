e8490349ee2e640473fc96a094d21c9e4a7c1a7a
Nel <nel.renaudin@gmail.com>
2022-09-28 13:19:16 +0200

    Create player factory function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('working');
+function playerMaker(name, mark) {
+  let score = 0;
+  const getName = () => name;
+  const getMark = () => mark;
+  const getScore = () => score;
+  const incrementScore = () => score++;
+
+  return {
+    getName,
+    getMark,
+    getScore,
+    incrementScore
+  };
+};
